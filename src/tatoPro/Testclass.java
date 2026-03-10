08e226062f64569d6a55e5e3192e310d2846da45
SCLPT 123 <SCLPT 123@192.168.1.3>
2026-03-10 13:18:30 +0530

    This is my first commit

---
+++ src/tatoPro/Testclass.java
@@ -0,0 +1,18 @@
+package tatoPro;
+
+public class Testclass {
+
+	public static void main(String[] args) {
+		String hungry="yes";
+		
+		if(hungry=="yes")
+			System.out.println("Eat something....");
+		else
+		{
+			System.out.println("Work then....");
+			
+		}
+
+	}
+
+}

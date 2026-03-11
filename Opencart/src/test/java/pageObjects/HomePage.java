b1e0a19967770184074e1f0598ee055e0b1493f4
unknown <priyawonder6@gmail.com>
2026-03-11 09:29:57 +0530

    This is my first commit

---
+++ Opencart/src/test/java/pageObjects/HomePage.java
@@ -0,0 +1,44 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends BasePage {
+	
+	
+	public HomePage(WebDriver driver) {
+		
+		super(driver);
+		
+		
+	}
+	
+	
+@FindBy(xpath="//span[text()='My Account']")
+WebElement lnkmyAccount;
+
+@FindBy(xpath="//a[text()='Register']")
+WebElement lnkmyRegister;
+
+public void clickmyAccount() {
+	 lnkmyAccount.click();
+}
+
+public void clickmyRegister() {
+	 lnkmyRegister.click();
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ PetStoreAutomation/src/test/java/api/endpoints/UserEndPoints.java
@@ -0,0 +1,19 @@
+package api.endpoints;
+
+//used to perform CRUD operations
+
+/*
+ * Create,Read,Update,Delete requests
+ */
+
+
+public class UserEndPoints {
+
+	
+	
+	
+	
+	
+	
+	
+}
